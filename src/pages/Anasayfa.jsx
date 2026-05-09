12f983c3a4c125f942bf14d80626d47b579aa7dc
EgeYesil <egebrkk@gmail.com>
2026-05-09 15:37:59 +0300

    anasayfa css

--- src/pages/Anasayfa.jsx
+++ src/pages/Anasayfa.jsx
@@ -1,20 +1,22 @@
 import { useNavigate } from 'react-router-dom'
+import './Anasayfa.css'
+import logo from '../assets/logo.svg'
+import banner from '../../images/iteration-1-images/home-banner.png'
 
 function Anasayfa() {
   const navigate = useNavigate()
 
   return (
-    <div>
-      <header>
-        <h3>Teknolojik Yemekler</h3>
-      </header>
-
-      <main>
+    <div className="anasayfa">
+    <header>
+        <img src={logo} alt="Teknolojik Yemekler" />
+    </header>
+    <main>
         <h1>KOD ACIKTIR<br />PİZZA, DOYURUR</h1>
-        <button onClick={() => navigate('/siparis')}>
-          ACIKTIM
+        <button className="aciktim-btn" onClick={() => navigate('/siparis')}>
+        ACIKTIM
         </button>
-      </main>
+    </main>
     </div>
   )
 }
